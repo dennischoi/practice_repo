76d21d5e9e27d2c1763effc23068b6c17a7d96e6
Dennis Choi <dennis.choi7@gmail.com>
2016-07-09 21:07:33 -0400

    - Basic TTT implemented 	- JS/jQuery library added 		- functions implemented for interactive functionality
    - CSS styling for basic board

--- js/ttt.js
+++ js/ttt.js
@@ -1,8 +1,29 @@
-$(document).on('ready, page:load', function() {
+
+$(document).on('ready', function() {
 
 
+  var turn = 0;
+  // var board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  //
+  //
 
+    function clickBox(ev) {
+        console.log('pressed')
+            if (turn % 2 === 0) {
+                $(this).html('O').addClass('o');
+            }else{
+                $(this).html('X').addClass('x');
+            }
 
+            //  FUNCTION TO CHECK WINNER
+            // checkWinner();
+            $(this).off('click')
+
+            turn++
+            console.log(turn)
+    };
+
+  $('td').on('click', clickBox)
 
 
 
